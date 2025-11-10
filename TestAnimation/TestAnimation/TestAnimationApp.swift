8f1f643333a34e5ada7364bff887a01bf459f28a
RuslanWorkGit <lulka758@gmail.com>
2025-11-10 23:24:06 +0200

    create proj

---
+++ TestAnimation/TestAnimation/TestAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAnimationApp.swift
+//  TestAnimation
+//
+//  Created by Ruslan Liulka on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
